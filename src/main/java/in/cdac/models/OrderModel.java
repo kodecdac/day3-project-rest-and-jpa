a30d6fa5bff92b0688ef8d1268f5efd1cd3e0646
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-02-23 15:39:42 +0530

    workign with swagger ui.

--- src/main/java/in/cdac/models/OrderModel.java
+++ src/main/java/in/cdac/models/OrderModel.java
@@ -10,9 +10,6 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
