fd1dca5c3a3ce3cf8114be943ca3097e66cf1d00
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-02-24 08:51:43 +0530

    jwt utils.

--- src/main/java/in/cdac/controller/OrderController.java
+++ src/main/java/in/cdac/controller/OrderController.java
@@ -1,7 +1,11 @@
 package in.cdac.controller;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -20,6 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import in.cdac.models.OrderModel;
 import in.cdac.repository.OrderModelRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Validated
 @RestController
@@ -79,5 +85,16 @@ public class OrderController {
 	@GetMapping("/hi")
 	public String hi() {
 		return "Hi...";
+	}
+	
+	@GetMapping("/jwt")
+	public String jwt() {
+		
+		return Jwts.builder()
+					.claim("name", "xyz")
+					.signWith(SignatureAlgorithm.HS256, "secret")
+					.setIssuedAt(new Date())
+					.setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 24 * 1000))
+					.compact();
 	}
 }
